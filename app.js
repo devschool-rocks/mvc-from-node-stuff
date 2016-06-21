0a40e13235cd581f94c274a6623077c677e21a69
Jim OKelly <jim@devschool.rocks>
2016-06-21 16:24:58 -0500

    starting the framework for a database

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
+require('dotenv').config();
+
 var app = require('express')();
 
 var initApp = function (app) {
+  initKnex();
+  initExpress(app);
+};
+
+var initExpress = function (app) {
   app.set('view engine', 'pug');
   app.set('views', "./app/views");
   app.listen(3000);
+};
+
+var initKnex = function () {
 };
 
 app.get('/', function (req, res) {
---
+++ knexfile.js
@@ -0,0 +1,24 @@
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection: {
+      database: process.env.DB_NAME,
+      user:     process.env.DB_USER,
+      password: process.env.DB_PASS
+    },
+    pool: {
+      min: 1,
+      max: 2
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: 'db/migrations'
+    },
+    seeds: {
+      directory: 'db/seeds'
+    },
+  },
+
+};
+
